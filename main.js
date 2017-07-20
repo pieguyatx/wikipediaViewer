f797bd90ef999e72cc7aee468b351333d53e3e9b
Pius Wong <pius@pioslabs.com>
2017-07-20 16:10:41 -0500

    Make note to add random search button later

--- main.js
+++ main.js
@@ -8,4 +8,12 @@ $(document).ready(function(){
     // allow draggability: http://api.jqueryui.com/draggable/
     // inform user of draggability, clickability (popup? snackbar?)
   });
+  $("#random").on("click", function(){
+    // fill in code
+    console.log("random button clicked"); //debug
+    // call API data
+    // parse data; distribute into boxes
+    // allow draggability: http://api.jqueryui.com/draggable/
+    // inform user of draggability, clickability (popup? snackbar?)
+  });
 });
