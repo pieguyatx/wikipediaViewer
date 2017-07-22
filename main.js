3db0042cbd6f4a99c45f0e152feb5aeceab8a4e7
Pius Wong <pius@pioslabs.com>
2017-07-22 16:33:57 -0500

    Refactor. Move functions outside "document ready" stuff

--- main.js
+++ main.js
@@ -20,25 +20,25 @@ $(document).ready(function(){
     // use similar functions as previously defined to display data
   });
 
-  // search for user's custom input terms
-  function searchCustom(){
-    // get user search terms using Opensearch API: https://www.mediawiki.org/wiki/API:Opensearch
-    var limit = 8; // number of entries to obtain
-    var searchTerms = document.getElementById("searchInput").value;
-    console.log('Button clicked. Searching for: ' + searchTerms); // debug
-    var searchAPI = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerms + "&limit=" + limit + "&namespace=0&format=json&callback=?&redirects=resolve";
-    $.getJSON(searchAPI, function(searchData){
-      if(searchData.hasOwnProperty("error")){
-        console.log("Invalid search terms"); // debug
-      }
-      else{
-        console.log(searchData); //debug
-      }
-    });
-    // call API data: https://www.mediawiki.org/wiki/API:Main_page
-    // parse data; distribute into boxes
-    // allow draggability: http://api.jqueryui.com/draggable/
-    // inform user of draggability, clickability (popup? snackbar?)
-  }
-
 });
+
+// search for user's custom input terms
+function searchCustom(){
+  // get user search terms using Opensearch API: https://www.mediawiki.org/wiki/API:Opensearch
+  var limit = 8; // number of entries to obtain
+  var searchTerms = document.getElementById("searchInput").value;
+  console.log('Button clicked. Searching for: ' + searchTerms); // debug
+  var searchAPI = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerms + "&limit=" + limit + "&namespace=0&format=json&callback=?&redirects=resolve";
+  $.getJSON(searchAPI, function(searchData){
+    if(searchData.hasOwnProperty("error")){
+      console.log("Invalid search terms"); // debug
+    }
+    else{
+      console.log(searchData); //debug
+    }
+  });
+  // call API data: https://www.mediawiki.org/wiki/API:Main_page
+  // parse data; distribute into boxes
+  // allow draggability: http://api.jqueryui.com/draggable/
+  // inform user of draggability, clickability (popup? snackbar?)
+}
