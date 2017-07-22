525b2e248efdd171e2a88dc46fcb641a0b0ebb67
Pius Wong <pius@pioslabs.com>
2017-07-22 16:27:41 -0500

    Add search responsiveness to keyboard for input text box.

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
+
+  // search wikipedia on keyboard press (enter/return)
+  $("#searchInput").on('keyup', function (e) {
+    if (e.keyCode == 13) {
+      searchCustom();
+    }
+  });
+
   // search wikipedia on click
   $("#searchButton").on("click", function(){
+    searchCustom();
+  });
+
+  // search wikipedia randomly on click
+  $("#random").on("click", function(){
+    // fill in code
+    console.log("random button clicked"); //debug
+    // call API data: https://en.wikipedia.org/wiki/Special:Random
+    // use similar functions as previously defined to display data
+  });
+
+  // search for user's custom input terms
+  function searchCustom(){
     // get user search terms using Opensearch API: https://www.mediawiki.org/wiki/API:Opensearch
     var limit = 8; // number of entries to obtain
     var searchTerms = document.getElementById("searchInput").value;
@@ -18,11 +39,6 @@ $(document).ready(function(){
     // parse data; distribute into boxes
     // allow draggability: http://api.jqueryui.com/draggable/
     // inform user of draggability, clickability (popup? snackbar?)
-  });
-  $("#random").on("click", function(){
-    // fill in code
-    console.log("random button clicked"); //debug
-    // call API data: https://en.wikipedia.org/wiki/Special:Random
-    // use similar functions as previously defined to display data
-  });
+  }
+
 });
